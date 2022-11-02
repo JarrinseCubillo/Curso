c916efe215ebbd9c6ee48074fff02a49a141bf6a
Jarrinse C <javierjjcccastillo@hotmail.com>
2022-11-01 17:00:26 -0700

    Practica Carrito y tests

--- depot/spec/models/products_spec.rb
+++ depot/spec/models/products_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 # https://semaphoreci.com/community/tutorials/how-to-test-rails-models-with-rspec
 RSpec.describe Product, :type => :model do
-  subject { described_class.new }
+  subject { build(:product_1) }
 
   it "is valid with valid attributes" do
     subject.title = "Anything"
@@ -17,17 +17,23 @@ RSpec.describe Product, :type => :model do
   end
 
   it "is not valid without a description" do
-    product = Product.new(description: nil)
-    expect(product).to_not be_valid
+    subject.description = nil
+    expect(subject).to_not be_valid
   end
 
   it "is not valid without a price" do
-    product = Product.new(price: nil)
-    expect(product).to_not be_valid
+    subject.price = nil
+    expect(subject).to_not be_valid
   end
 
   it "is not valid with a price with -1" do
-    product = Product.new(price: -1)
-    expect(product).to_not be_valid
+    subject.price = -1
+    expect(subject).to_not be_valid
+  end
+
+  describe 'is not valid without a uniqueness title' do
+    before { create(:product_1) }
+
+    it { expect(subject).to_not be_valid }
   end
 end
